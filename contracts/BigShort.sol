25396cbc4dfc1d18c342041c3b816db588905699
danetteceola <danetteceola@gmail.com>
2022-10-12 21:00:56 +0800

    initial

---
+++ contracts/BigShort.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+
+contract BigShortAlpha {
+
+    AggregatorV3Interface internal priceFeed;
+
+    uint public unlockTime;
+    address payable public owner;
+
+    int public pricePrediction;
+
+    event Withdrawal(uint amount, uint when);
+
+    /**
+     * Network: Goerli
+     * Aggregator: ETH/USD
+     * Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+    */
+    constructor(uint _unlockTime, int _pricePrediction) payable {
+        require(
+            block.timestamp < _unlockTime,
+            "Unlock time should be in the future"
+        );
+
+        unlockTime = _unlockTime;
+        pricePrediction = _pricePrediction;
+        owner = payable(msg.sender);
+        priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419); // mainnet
+        //priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    function withdraw() public {
+        console.log("time is %s, unlockTime is %s", block.timestamp, unlockTime);
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        require(msg.sender == owner, "You aren't the owner");
+
+        int priceNow = this.getLatestPrice();
+
+        console.log("Price is %s, prediction is %s", uint(priceNow), uint(pricePrediction));
+
+        require(priceNow > pricePrediction, "price less than prediction");
+
+        emit Withdrawal(address(this).balance, block.timestamp);
+        owner.transfer(address(this).balance);
+    }
+}
---
+++ test/testBigShort.js
@@ -0,0 +1,130 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+describe("BigShort", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployOneYearLockFixture() {
+    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+    const ONE_GWEI = 1_000_000_000;
+
+    const lockedAmount = ONE_GWEI;
+    const unlockTime = (await time.latest()) + 60;
+
+    const predictionPrice = 1500;
+
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Contract = await ethers.getContractFactory("BigShortAlpha");
+    const contract = await Contract.deploy(unlockTime, predictionPrice,  { value: lockedAmount });
+
+    return { contract, unlockTime, lockedAmount, predictionPrice, owner, otherAccount };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right unlockTime", async function () {
+      const { contract, unlockTime } = await loadFixture(deployOneYearLockFixture);
+
+      expect(await contract.unlockTime()).to.equal(unlockTime);
+    });
+
+    it("Should set the right owner", async function () {
+      const { contract, owner } = await loadFixture(deployOneYearLockFixture);
+
+      expect(await contract.owner()).to.equal(owner.address);
+    });
+
+    it("Should receive and store the funds to lock", async function () {
+      const { contract, lockedAmount } = await loadFixture(
+        deployOneYearLockFixture
+      );
+
+      expect(await ethers.provider.getBalance(contract.address)).to.equal(
+        lockedAmount
+      );
+    });
+
+    it("Should fail if the unlockTime is not in the future", async function () {
+      // We don't use the fixture here because we want a different deployment
+      const latestTime = await time.latest();
+      const Contract = await ethers.getContractFactory("BigShortAlpha");
+      const predictionPrice = 1500;
+      await expect(Contract.deploy(latestTime, predictionPrice, { value: 1 })).to.be.revertedWith(
+        "Unlock time should be in the future"
+      );
+    });
+  });
+
+  describe("Withdrawals", function () {
+    describe("Validations", function () {
+      it("Should revert with the right error if called too soon", async function () {
+        const { contract } = await loadFixture(deployOneYearLockFixture);
+
+        await expect(contract.withdraw()).to.be.revertedWith(
+          "You can't withdraw yet"
+        );
+      });
+
+      it("Should revert with the right error if called from another account", async function () {
+        const { contract, unlockTime, otherAccount } = await loadFixture(
+          deployOneYearLockFixture
+        );
+
+        // We can increase the time in Hardhat Network
+        await time.increaseTo(unlockTime);
+
+        // We use lock.connect() to send a transaction from another account
+        await expect(contract.connect(otherAccount).withdraw()).to.be.revertedWith(
+          "You aren't the owner"
+        );
+      });
+
+      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
+        const { contract, unlockTime } = await loadFixture(
+          deployOneYearLockFixture
+        );
+        console.log(unlockTime);
+        console.log(await contract.getLatestPrice());
+        // Transactions are sent using the first signer by default
+        await time.increaseTo(unlockTime);
+
+        await expect(contract.withdraw()).not.to.be.reverted;
+      });
+    });
+
+    describe("Events", function () {
+      it("Should emit an event on withdrawals", async function () {
+        const { contract, unlockTime, lockedAmount } = await loadFixture(
+          deployOneYearLockFixture
+        );
+
+        await time.increaseTo(unlockTime);
+
+        await expect(contract.withdraw())
+          .to.emit(contract, "Withdrawal")
+          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
+      });
+    });
+
+    describe("Transfers", function () {
+      it("Should transfer the funds to the owner", async function () {
+        const { contract, unlockTime, lockedAmount, owner } = await loadFixture(
+          deployOneYearLockFixture
+        );
+
+        await time.increaseTo(unlockTime);
+
+        await expect(contract.withdraw()).to.changeEtherBalances(
+          [owner, contract],
+          [lockedAmount, -lockedAmount]
+        );
+      });
+    });
+  });
+});
